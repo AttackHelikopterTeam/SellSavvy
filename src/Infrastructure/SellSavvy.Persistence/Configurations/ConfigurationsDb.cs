981b0ee6b2751b32f809971f958b40f41cd23b1a
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-12-31 04:49:03 +0300

    [Update]: Added Database Configuration and Context Setup
    - Implemented configuration management for the database connection string using the ConfigurationsDb class.
    - Created a SellSavvyIdentityContext to represent the application's database context, extending IdentityDbContext for user and role management.
    - Added DbSet properties for Category, Product, ProductCategory, and ProductPerson entities.
    - Configured entity relationships and applied Fluent API configurations in the OnModelCreating method of SellSavvyIdentityContext.
    - Implemented a design-time context factory (SellSavvyIdentityContextFactory) for generating the database context during design-time.

---
+++ src/Infrastructure/SellSavvy.Persistence/Configurations/ConfigurationsDb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SellSavvy.Persistence.Configurations
+{
+    public static class ConfigurationsDb
+    {
+        public static string GetString(string key)
+        {
+            ConfigurationManager configurationManager = new();
+
+            string path = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName}\\Infrastructure\\SellSavvy.Persistence\\Configurations";
+
+            configurationManager.SetBasePath(path);
+
+            configurationManager.AddJsonFile("PrivateInformations.json"); 
+
+            return configurationManager.GetSection(key).Value;
+        }
+
+    }
+}
---
+++ src/Infrastructure/SellSavvy.Persistence/Contexts/SellSavvyIdentityContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SellSavvy.Persistence.Configurations;
+
+namespace SellSavvy.Persistence.Contexts
+{
+   public class SellSavvyIdentityContextFactory : IDesignTimeDbContextFactory<SellSavvyIdentityContext>
+        {
+            public SellSavvyIdentityContext CreateDbContext(string[] args)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var optionsBuilder = new DbContextOptionsBuilder<SellSavvyIdentityContext>();
+
+                var connectionString = ConfigurationsDb.GetString("ConnectionStrings:PostgreSQL");
+
+                optionsBuilder.UseNpgsql(connectionString);
+
+                return new SellSavvyIdentityContext(optionsBuilder.Options);
+            }
+        }
+
+}
