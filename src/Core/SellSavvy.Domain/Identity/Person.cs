04199cc6aff4bfb27a381fa463974c6acc09e3bb
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-12-31 04:34:21 +0300

    [Feature]: Added Person and Role Entities to Identity
    - Created the 'Person' class, extending IdentityUser<Guid>, to represent individuals with properties like FullName, Gender, and a list of associated ProductPersons.
    - Introduced the 'Role' class, extending IdentityRole<Guid>, to define roles within the identity system.
    -Anil

---
+++ src/Core/SellSavvy.Domain/Identity/Person.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SellSavvy.Domain.Entities;
+using SellSavvy.Domain.Enum;
+
+namespace SellSavvy.Domain.Identity
+{
+    public class Person : IdentityUser<Guid>
+    {
+        public string? FullName { get; set; }
+        public Gender Gender { get; set; }
+        public List<ProductPerson> ProductPersons { get; set; } = new List<ProductPerson>();
+    }
+}
