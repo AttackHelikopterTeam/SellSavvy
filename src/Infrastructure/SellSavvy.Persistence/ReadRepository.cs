8d67c581fece62a2f586d024871cf9f06e4a90ba
Seyma <sseymacakmakci@gmail.com>
2024-01-06 19:57:24 +0300

    [Feat] Added ReadRepository and WriteRepository

--- src/Infrastructure/SellSavvy.Persistence/ReadRepository.cs
+++ src/Infrastructure/SellSavvy.Persistence/ReadRepository.cs
@@ -1,11 +1,69 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using SellSavvy.Domain.Common;
+using SellSavvy.Application;
+
+using SellSavvy.Persistence.Contexts;
+
 namespace SellSavvy.Persistence
 {
-	public class ReadRepository
-	{
-		public ReadRepository()
-		{
-		}
-	}
+    public class ReadRepository<T, Tkey> : SellSavvy.Domain.Common.IReadRepository<T, Tkey> where T : EntityBase<Tkey>
+    {
+        private readonly SellSavvyIdentityContext _context;
+
+        public ReadRepository(SellSavvyIdentityContext context)
+        {
+            _context = context;
+        }
+
+        private DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll()
+        {
+            return Table.AsQueryable();
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).AsQueryable();
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate, bool trackChanges)
+        {
+            return trackChanges ? Table.Where(predicate).AsQueryable() : Table.AsNoTracking().Where(predicate).AsQueryable();
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> predicate, bool trackChanges)
+        {
+            return trackChanges ? Table.Where(predicate).AsQueryable() : Table.AsNoTracking().Where(predicate).AsQueryable();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await Table.FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<T?> GetByIdAsync(Tkey id)
+        {
+            if (typeof(T).GetProperty("Id").PropertyType == typeof(Guid))
+            {
+                // Assuming Id is of type Guid
+                return await Table.FirstOrDefaultAsync(x => x.Id.ToString() == id.ToString());
+            }
+            else if (typeof(T).GetProperty("Id").PropertyType == typeof(int))
+            {
+                // Assuming Id is of type int
+                if (int.TryParse(id.ToString(), out int parsedId))
+                {
+                    return await Table.FirstOrDefaultAsync(x => x.Id.Equals(parsedId));
+                }
+            }
+
+            // Handle other types of Id if necessary
+            return null;
+        }
+    }
 }
 
--- src/Infrastructure/SellSavvy.Persistence/WriteRepository.cs
+++ src/Infrastructure/SellSavvy.Persistence/WriteRepository.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SellSavvy.Application;
+using SellSavvy.Domain.Common;
+using SellSavvy.Persistence.Contexts;
+
 namespace SellSavvy.Persistence
 {
-	public class WriteRepository
-	{
-		public WriteRepository()
-		{
-		}
-	}
+    public class WriteRepository<T, TKey> : IWriteRepository<T, TKey> where T : EntityBase<TKey>
+
+    {
+        private readonly SellSavvyIdentityContext _context;
+
+        public WriteRepository(SellSavvyIdentityContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public async Task RemoveAsync(TKey id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity != null)
+                _context.Set<T>().Remove(entity);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
 }
 
